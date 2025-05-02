eebed9e8ae0d4a2037a43b7ea319f41dee888f5e
jmcg0192 <94491264+jmcg0192@users.noreply.github.com>
2025-05-01 20:17:42 -0400

    Add files via upload

--- getJSONData.js
+++ getJSONData.js
@@ -1,25 +1,22 @@
 // Does not need to be edited
 
 const getJSONData = async () => {
-	// Replace YOUR_BIN_ID with the actual ID of your bin
 	const binId = '68124a478561e97a500ae25e';
-	// Replace YOUR_API_KEY with your JSONBin.io API key
 	const apiKey = '$2a$10$4gNjie2gLr.zVaIgMqTnuO79WGbCT4MSeM7jEejg8GiLpR.RnnQJ6';
 	const url = `https://api.jsonbin.io/v3/b/${binId}?meta=false`;
   
 	const response = await fetch(url, {
-	  method: 'GET',
-	  headers: {
-		'X-Master-Key': apiKey,
-		'Content-Type': 'application/json'
-	  }
+		method: 'GET',
+		headers: {
+			'X-Master-Key': apiKey,
+			'Content-Type': 'application/json'
+		}
 	});
   
 	if (response.status !== 200) {
-	  throw new Error("Cannot fetch data");
+		throw new Error("Cannot fetch data");
 	}
   
-	let data = await response.json();
-	return JSON.stringify(data);
-  };
-  
+	const data = await response.json(); // data is already an object
+	return data; // ⬅️ FIX: remove stringify
+};
